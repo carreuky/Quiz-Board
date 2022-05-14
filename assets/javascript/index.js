ef3d1dccb1d4cf643c868072e3c373de935dc46e
Moringa School <carreiky@gmail.com>
2022-05-14 23:30:59 +0300

    enabling to go next

---
+++ assets/javascript/index.js
@@ -0,0 +1,34 @@
+document.getElementsByClassName("questions")[0].style.display="block";
+
+function next1() {
+    document.getElementsByClassName("questions")[0].style.display = "none";
+    document.getElementsByClassName("questions")[1].style.display = "block";
+    document.getElementsByClassName("questions")[2].style.display = "none";
+    document.getElementsByClassName("questions")[3].style.display = "none";
+    document.getElementsByClassName("questions")[4].style.display = "none";
+    return false;
+}
+function next2() {
+    document.getElementsByClassName('questions')[0].style.display = "none";
+    document.getElementsByClassName('questions')[1].style.display = "none";
+    document.getElementsByClassName('questions')[2].style.display = "block";
+    document.getElementsByClassName('questions')[3].style.display = "none";
+    document.getElementsByClassName('questions')[4].style.display = "none";
+    return false;
+}
+function next3() {
+    document.getElementsByClassName("questions")[0].style.display = "none";
+    document.getElementsByClassName("questions")[1].style.display = "none";
+    document.getElementsByClassName("questions")[2].style.display = "none";
+    document.getElementsByClassName("questions")[3].style.display = "block";
+    return false;
+}
+function next4() {
+    document.getElementsByClassName('questions')[0].style.display = "none";
+    document.getElementsByClassName('questions')[1].style.display = "none";
+    document.getElementsByClassName('questions')[2].style.display = "none";
+    document.getElementsByClassName('questions')[3].style.display = "none";
+    document.getElementsByClassName('questions')[4].style.display = "block";
+    document.getElementsByClassName('submitClass')[0].style.display = "block";
+    return false;
+}
